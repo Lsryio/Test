137ad5b5fa7fe3944b415d5e7a24e061c41a543b
Lsryio <37201334+Lsryio@users.noreply.github.com>
2018-04-17 15:17:08 +0930

    Update db.php

--- db.php
+++ db.php
@@ -2,5 +2,5 @@
 {\fonttbl}
 {\colortbl;\red255\green255\blue255;}
 {\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0 //www.youtube.com
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0 //http://www.youtube.com
 }
