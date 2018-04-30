e06d37155c31b71ce74add62e5409bb636493eed
Lsryio <37201334+Lsryio@users.noreply.github.com>
2018-04-30 18:02:42 +0930

    Update test 2.php

--- test 2.php
+++ test 2.php
@@ -1,7 +1,8 @@
-{\rtf1\ansi\ansicpg936\cocoartf1504\cocoasubrtf830
-{\fonttbl}
-{\colortbl;\red255\green255\blue255;}//kk http://www.youtube.com
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0 //http://www.github.com
-}
-<?php phpInfo(); ?>// bkhfg http://www.github.com
+<html>
+<head>
+<title>PHP test</title>
+</head>
+<body>
+<?php echo '<p>Hello World</p>'; ?>
+</body>
+</html>
