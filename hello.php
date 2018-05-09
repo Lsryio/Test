cb2ae3ecb4d71053d455441d1a1769408bbd0f8e
Lsryio <37201334+Lsryio@users.noreply.github.com>
2018-05-09 15:48:54 +0930

    Delete hello.php

--- hello.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-<title>PHP </title>
-</head>
-<body>
-<?php echo '<p>Hello World</p>'; ?> // https://github.com/
-</body>https://sites.google.com/site/mci09test/
-</html>//https://sites.google.com/view/lsryio123/
